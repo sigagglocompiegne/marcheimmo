0929828488f2ffd4720a364fe26091f0e37be339
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-04-14 17:11:08 +0200

    Update IMMO_10_squelette.sql

--- sql/IMMO_10_squelette.sql
+++ sql/IMMO_10_squelette.sql
@@ -42,8 +42,8 @@ DROP TABLE IF EXISTS m_economie.lk_immo_bienbati CASCADE;
 -- DOMAINES DE VALEUR
 
 DROP TABLE IF EXISTS m_economie.lt_immo_ityp CASCADE;
-DROP TABLE IF EXISTS m_economie.lt_immo_bdesc CASCADE;
-DROP TABLE IF EXISTS m_economie.lt_immo_bbati CASCADE;
+DROP TABLE IF EXISTS m_economie.lt_immo_dbien CASCADE;
+DROP TABLE IF EXISTS m_economie.lt_immo_dbati CASCADE;
 DROP TABLE IF EXISTS m_economie.lt_immo_tbien CASCADE;
 DROP TABLE IF EXISTS m_economie.lt_immo_etat CASCADE;
 
@@ -92,17 +92,59 @@ DROP TRIGGER IF EXISTS  ON m_economie. ;
 
 --############################################################ lt_immo_ityp ##################################################
 
+CREATE TABLE m_reseau_sec.lt_immo_ityp
+(
+  code character varying(2) NOT NULL,
+  valeur character varying(80) NOT NULL,
+  CONSTRAINT lt_immo_ityp_pkey PRIMARY KEY (code)
+)
+WITH (
+  OIDS=FALSE
+);
 
 INSERT INTO m_economie.lt_immo_ityp(code, valeur)
     VALUES
-	('10','Terrain virger'),
-	('20','..'),
-	('21','..');
+	('10','Terrain vierge'),
+	('21','Local (Bâtiment non divisé)'),
+	('22','Local indépendant divisé'),
+	('23','Local non identifié dans un bâtiment divisible');
 
 COMMENT ON TABLE m_economie.lt_immo_ityp
   IS 'Code permettant de décrire le type d''objet saisie';
-COMMENT ON COLUMN m_economie.lt_immo_ityp.code IS 'Code de la liste de l''objet saisi';
-COMMENT ON COLUMN m_economie.lt_immo_ityp.valeur IS 'Valeur de la liste l''objet saisi';
+COMMENT ON COLUMN m_economie.lt_immo_ityp.code IS 'Code du type d''objet immobilier saisi';
+COMMENT ON COLUMN m_economie.lt_immo_ityp.valeur IS 'Valeur du type d''objet immobilier saisi';
+
+--############################################################ lt_immo_dbati ##################################################
+
+CREATE TABLE m_reseau_sec.lt_immo_dbati
+(
+  code character varying(2) NOT NULL,
+  valeur character varying(80) NOT NULL,
+  CONSTRAINT lt_immo_dbati_pkey PRIMARY KEY (code)
+)
+WITH (
+  OIDS=FALSE
+);
+
+INSERT INTO m_economie.lt_immo_dbati(code, valeur)
+    VALUES
+	('0','Non renseigné'),
+	('1','Double vitrage'),
+	('2','Site clôturé'),
+	('3','Places de parking'),
+	('4','Murs périphériques (bardage métallique…)'),
+	('5','Ossature (couverture bac acier…)'),
+	('6','Menuiserie aluminium'),
+	('7','Accès sécurisé'),
+	('8','Système d''alarme'),
+	('9','Parties communes'),
+	('10','Portes de plain-pied),
+	('11','Charge au sol);
+
+COMMENT ON TABLE m_economie.lt_immo_dbati
+  IS 'Code permettant de décrire la description du bien';
+COMMENT ON COLUMN m_economie.lt_immo_dbati.code IS 'Code du type décrivant des éléments du bâtiment';
+COMMENT ON COLUMN m_economie.lt_immo_dbati.valeur IS 'Valeur du type décrivant des éléments du bâtiment';
 
 	
 -- ###############################################################################################################################
