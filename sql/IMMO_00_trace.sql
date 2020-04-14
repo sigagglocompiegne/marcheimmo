a69e42e1c00ccd34eed677dc81feb564852f57b5
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-04-14 15:22:27 +0200

    Create IMMO_00_trace.sql

---
+++ sql/IMMO_00_trace.sql
@@ -0,0 +1,18 @@
+/*IMMO V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* IMMO_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Bodet Grégory */
+
+/*  
+ 
+  Liste des dépendances :
+  schéma          | table                 | description                                                   | usage
+*/
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2020-04-14 : GB / initialisation du code (1ère version d'essai d'un fonctionnel)
+
