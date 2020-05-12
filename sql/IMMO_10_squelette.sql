b8ea05dbd0fe0c0ffb6493353843695534eb1f41
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-05-12 08:26:29 +0200

    Update IMMO_10_squelette.sql

--- sql/IMMO_10_squelette.sql
+++ sql/IMMO_10_squelette.sql
@@ -848,7 +848,34 @@ COMMENT ON COLUMN m_economie.an_immo_media.date_sai
 COMMENT ON COLUMN m_economie.an_immo_media.gid
     IS 'Compteur (identifiant interne)';
 
+--################################################################# an_vmr_immo_ityp #######################################################
 
+					 
+CREATE MATERIALIZED VIEW m_economie.an_vmr_immo_ityp AS
+SELECT
+bi.idimmo,
+bi.ityp as ityp_objet,
+ba.ityp as ityp_bati
+FROM
+m_economie.geo_immo_bien bi,
+m_economie.an_immo_bati ba
+WHERE
+bi.idimmo = ba.idimmo
+AND
+bi.ityp <> '22'
+WITH DATA;
+
+ALTER TABLE m_economie.an_vmr_immo_ityp
+    OWNER TO sig_create;
+
+COMMENT ON MATERIALIZED VIEW m_economie.an_vmr_immo_ityp
+    IS 'Vue non géographiques listant les types d''occupation entre la table des objeys saisis et les attributs des bâtiments (exclu des bâtiments avec biens identifiés). Objectif : utiliser cette vue rafraichie après l''insertion d''un bâtiment pour mettre à jour cette même table des bâtis pour gérer la liste des bâtiments (uniquement si locaux identifiés) affichés à l''utilisateur dans GEO pour affecter un bâtiment à un bien identifié';
+
+GRANT DELETE, UPDATE, SELECT, INSERT ON TABLE m_economie.an_vmr_immo_ityp TO edit_sig;
+GRANT ALL ON TABLE m_economie.an_vmr_immo_ityp TO sig_create;
+GRANT ALL ON TABLE m_economie.an_vmr_immo_ityp TO create_sig;
+GRANT SELECT ON TABLE m_economie.an_vmr_immo_ityp TO read_sig;
+					 
 -- ###############################################################################################################################
 -- ###                                                                                                                         ###
 -- ###                                                        CONTRAINTES                                                      ###
