e7ad67266d8893e6c4173112cfc3f5198171f367
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-01-22 10:00:03 +0100

    Update IMMO_00_trace.sql

--- bdd/IMMO_00_trace.sql
+++ bdd/IMMO_00_trace.sql
@@ -19,3 +19,4 @@
 2020-05-05 : GB / début du développement de fonctions triggers pour générer les automatismes fonctionnelles
 2020-01-19 : GB / refonte complète de la gestion des données du patrimoine cartographique des bâtiments et locaux d'activités (vue de gestion + fonction trigger)
 2020-01-19 : GB / Mise à jour de structure initiale avec attributs métiers complémentaires et intégration dans les routines de gestion
+2020-01-22 : GB / Intégration de la géolocalisation des bâtiments sur la Base Adresse Locale (BAL), permettant de récupérer la liste des établissements (occupants) affectés sur la BAL
